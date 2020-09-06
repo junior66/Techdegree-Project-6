6e9353e12b3c0ab2c8e423f1c4d159a6dcce77cf
junior66 <jraidee@gmail.com>
2020-09-05 23:53:50 -0400

    Added variables and function stubs

---
+++ app.js
@@ -0,0 +1,43 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const startButton = document.querySelector('.btn_reset');
+let missed = 0;
+
+const phrases = ['Tree House', 
+                'Jazz Music',
+                'Brain Fart',
+                'Midnight Sun',
+                'Four Seasons'
+
+]
+
+// return a random phrase from an array
+const getRandomPhraseAsArray = arr => {
+
+}
+
+// adds the letters of a string to the display
+const addPhraseToDisplay = arr => {
+
+}
+
+// check if a letter is in the phrase
+const checkLetter = button => {
+
+}
+
+// check if the game has been won or lost
+const checkWin = () => {
+
+}
+
+// listen for the start game button to be pressed
+startButton.addEventListener('click', () => {
+    
+
+});
+
+// listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e => {
+
+});
